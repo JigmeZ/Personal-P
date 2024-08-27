a55741e0e9a4043d35fd9d0e4473fe0f4dd907b3
Baluthegoat <141105500+Baluthegoat@users.noreply.github.com>
2024-08-27 12:14:20 +0600

    compiled 5

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -2,34 +2,12 @@
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { CiMenuFries } from "react-icons/ci";
-
-const links = [
-  {
-    name: "home",
-    path: "/",
-  },
-  {
-    name: "About Me",
-    path: "/About",
-  },
-  {
-    name: "Projects",
-    path: "/Projects",
-  },
-  {
-    name: "Contact",
-    path: "/Contact",
-  },
-];
 
 const MobileNav = () => {
   const pathname = usePathname();
   return (
     <Sheet>
-      <SheetTrigger className="flex justify-center items-center">
-        <CiMenuFries className="text-[32px] text-green-500" />
-      </SheetTrigger>
+      <SheetTrigger className="flex justify-center items-center"></SheetTrigger>
       <SheetContent className="flex flex-col">
         <div>logo</div>
       </SheetContent>
